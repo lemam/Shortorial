e865774c879f74bb924f1a0b05eb51dd69c1f9e3
우지민 <0542say@naver.com>
2024-04-24 10:52:39 +0900

    Feat: uploadSns 레포지토리 생성
    S10P31B305-316

---
+++ back/sleep/src/main/java/com/sleep/sleep/shorts/repository/UploadSnsRepository.java
@@ -0,0 +1,7 @@
+package com.sleep.sleep.shorts.repository;
+
+import com.sleep.sleep.shorts.entity.UploadSns;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UploadSnsRepository extends JpaRepository<UploadSns,Integer> {
+}
