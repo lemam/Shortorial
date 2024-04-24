5a9d51d849fde068196b1c9724e15e73cba79678
우지민 <0542say@naver.com>
2024-04-24 10:52:17 +0900

    Feat: uploadShorts 레포지토리 생성
    S10P31B305-316

---
+++ back/sleep/src/main/java/com/sleep/sleep/shorts/repository/UploadShortsRepository.java
@@ -0,0 +1,7 @@
+package com.sleep.sleep.shorts.repository;
+
+import com.sleep.sleep.shorts.entity.UploadShorts;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UploadShortsRepository extends JpaRepository<UploadShorts,Integer> {
+}
