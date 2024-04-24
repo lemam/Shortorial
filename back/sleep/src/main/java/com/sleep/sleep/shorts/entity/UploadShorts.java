fbe69f1e718985a10d6941c8df54cf8937ff41ee
우지민 <0542say@naver.com>
2024-04-24 10:36:48 +0900

    Feat: UploadShorts entity 추가
    S10P31B305-316

--- back/sleep/src/main/java/com/sleep/sleep/shorts/entity/UploadShorts.java
+++ back/sleep/src/main/java/com/sleep/sleep/shorts/entity/UploadShorts.java
@@ -1,4 +1,14 @@
 package com.sleep.sleep.shorts.entity;
 
+import jakarta.persistence.*;
+
 public class UploadShorts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int upload_no;
+    @ManyToMany
+    public int member_no;
+    @ManyToMany
+    public int sns_no;
+    public String upload_url;
 }
