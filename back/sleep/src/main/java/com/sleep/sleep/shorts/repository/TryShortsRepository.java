7ec12572da4342942749754080867e43219c593a
우지민 <0542say@naver.com>
2024-04-24 10:51:48 +0900

    Feat: TryShorts 레포지토리 생성
    S10P31B305-316

---
+++ back/sleep/src/main/java/com/sleep/sleep/shorts/repository/TryShortsRepository.java
@@ -0,0 +1,7 @@
+package com.sleep.sleep.shorts.repository;
+
+import com.sleep.sleep.shorts.entity.TryShorts;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TryShortsRepository extends JpaRepository<TryShorts,Integer> {
+}
