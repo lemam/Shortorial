a5f8aa77afd1ae23d80dbe0d0ca9c023354b4a85
우지민 <0542say@naver.com>
2024-04-24 11:15:55 +0900

    Feat: SwaggerConfig 추가

---
+++ back/sleep/src/main/java/com/sleep/sleep/common/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.sleep.sleep.common.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("댄싱 API")
+                        .description("우린 춤을 출거에요")
+                        .version("1.0.0"));
+    }
+}
+
