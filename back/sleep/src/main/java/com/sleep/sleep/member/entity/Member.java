4fd152677452d43e1bc483673b8da0711748c24c
우지민 <0542say@naver.com>
2024-04-24 17:35:02 +0900

    Feat: TryShorts관련 entity 추가

--- back/sleep/src/main/java/com/sleep/sleep/member/entity/Member.java
+++ back/sleep/src/main/java/com/sleep/sleep/member/entity/Member.java
@@ -1,8 +1,12 @@
 package com.sleep.sleep.member.entity;
 
+import com.sleep.sleep.shorts.entity.TryShorts;
+import com.sleep.sleep.shorts.entity.UploadShorts;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
@@ -18,4 +22,8 @@ public class Member {
     private String  refreshToken;
     private String memberProfile;
     private String  memberTiktokLink;
+    @OneToMany(mappedBy="tryNo")
+    private List<TryShorts> tryShorts;
+    @OneToMany(mappedBy = "uploadNo")
+    private List<UploadShorts> uploadShorts;
 }
