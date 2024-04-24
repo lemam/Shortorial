9fb2cd53c4782a4c1ad69d3b8515a8d80ba95613
우지민 <0542say@naver.com>
2024-04-24 10:36:09 +0900

    Feat: TryShorts entity 추가
    S10P31B305-316

--- back/sleep/src/main/java/com/sleep/sleep/shorts/entity/TryShorts.java
+++ back/sleep/src/main/java/com/sleep/sleep/shorts/entity/TryShorts.java
@@ -1,4 +1,18 @@
 package com.sleep.sleep.shorts.entity;
 
+import jakarta.persistence.*;
+
 public class TryShorts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int try_no;
+    @ManyToOne
+    public int shorts_no;
+    @ManyToMany
+    public int member_no;
+    public int try_count;
+    public int try_complete;
+    public int try_max_time;
+    public int try_current_time;
+
 }
