fa45c1e328916794bca4afc31c724eadaa8acd0d
우지민 <0542say@naver.com>
2024-04-24 10:37:04 +0900

    Feat: UploadSns entity 추가
    S10P31B305-316

--- back/sleep/src/main/java/com/sleep/sleep/shorts/entity/UploadSns.java
+++ back/sleep/src/main/java/com/sleep/sleep/shorts/entity/UploadSns.java
@@ -1,4 +1,10 @@
 package com.sleep.sleep.shorts.entity;
 
+import jakarta.persistence.*;
+
 public class UploadSns {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int sns_no;
+    public String sns_name;
 }
