032b224c8b01a456777a06e32f2ee8aad7b48e72
우지민 <0542say@naver.com>
2024-04-26 10:49:41 +0900

    Feat: S3 delete 추가

--- back/sleep/src/main/java/com/sleep/sleep/s3/S3Controller.java
+++ back/sleep/src/main/java/com/sleep/sleep/s3/S3Controller.java
@@ -11,12 +11,12 @@ import java.io.IOException;
 @RestController
 @RequiredArgsConstructor
 public class S3Controller {
-    private final S3Service s3UploadService;
+    private final S3Service s3Service;
 
     @GetMapping("/s3/download/{fileName}")
     public ResponseEntity<?> downloadFile(@PathVariable String fileName) {
         try {
-            String filePath = s3UploadService.getPath(fileName);
+            String filePath = s3Service.getPath(fileName);
             return new ResponseEntity<String>(filePath, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<String>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
@@ -27,11 +27,20 @@ public class S3Controller {
     @PostMapping("/s3/upload")
     public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file, @RequestParam("fileName") String fileName) {
         try {
-            String uploadUrl = s3UploadService.uploadFile(file, fileName);
+            String uploadUrl = s3Service.uploadFile(file, fileName);
             return new ResponseEntity<String>(uploadUrl, HttpStatus.OK);
         } catch (IOException e) {
             return new ResponseEntity<String>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
         } catch (Exception e) {
+            return new ResponseEntity<String>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @DeleteMapping("/s3/delete/{fileName}")
+    public ResponseEntity<?> deleteFile(@PathVariable String fileName){
+        try {
+            s3Service.deleteFile(fileName);
+            return new ResponseEntity<String>("Successfully delete!",HttpStatus.OK);
+        }catch (Exception e) {
             return new ResponseEntity<String>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
