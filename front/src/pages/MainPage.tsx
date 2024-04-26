a4d9cdaa17d804e595e5dfe1d050c9c295de0ad0
임지은 <ljieun01@naver.com>
2024-04-26 11:12:22 +0900

    Feat: 메인페이지 그리드로 영상 나열 및 클릭시 연습페이지로 이동
    S10P31B305-238

--- front/src/pages/MainPage.tsx
+++ front/src/pages/MainPage.tsx
@@ -1,10 +1,61 @@
 import { Link } from "react-router-dom";
+import danceVideo from "../assets/sample.mp4";
+import styled from "styled-components";
 
 export default function MainPage() {
   return (
     <>
-      <div>메인 페이지</div>
+      <h1>Let's DANCE</h1>
+      <div
+        style={{
+          position: "relative",
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(162px, 1fr))",
+          gap: "16px 16px",
+        }}
+      >
+        <Link to={"/learn"}>
+          <VideoBox>
+            <div style={{ paddingTop: "177.8%" }}>
+              <div
+                style={{
+                  position: "absolute",
+                  left: 0,
+                  top: 0,
+                  width: "100%",
+                  height: "100%",
+                }}
+              >
+                <div style={{ width: "100%", height: "100%" }}>
+                  <video src={danceVideo}></video>
+                </div>
+              </div>
+            </div>
+          </VideoBox>
+        </Link>
+
+        <VideoBox>
+          <video src={danceVideo}></video>
+        </VideoBox>
+        <VideoBox>
+          <video src={danceVideo}></video>
+        </VideoBox>
+      </div>
       <Link to={"/learn"}>쇼츠를 눌러주세용</Link>
     </>
   );
 }
+
+const VideoBox = styled.div`
+  position: relative;
+  width: 100%;
+  max-width: 286px;
+  border-radius: 8px;
+  overflow: hidden;
+
+  video {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
