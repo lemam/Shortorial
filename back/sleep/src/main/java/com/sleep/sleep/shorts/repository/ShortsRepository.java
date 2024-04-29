2f2ccd3fd7ffc32c438e39a7e06e5a9279e8b13f
우지민 <0542say@naver.com>
2024-04-29 14:08:22 +0900

    Feat: 레자지토리 어노테이션 추가

--- back/sleep/src/main/java/com/sleep/sleep/shorts/repository/ShortsRepository.java
+++ back/sleep/src/main/java/com/sleep/sleep/shorts/repository/ShortsRepository.java
@@ -2,6 +2,8 @@ package com.sleep.sleep.shorts.repository;
 
 import com.sleep.sleep.shorts.entity.Shorts;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ShortsRepository extends JpaRepository<Shorts,Integer> {
 }
--- back/sleep/src/main/java/com/sleep/sleep/shorts/repository/TryShortsRepository.java
+++ back/sleep/src/main/java/com/sleep/sleep/shorts/repository/TryShortsRepository.java
@@ -2,6 +2,8 @@ package com.sleep.sleep.shorts.repository;
 
 import com.sleep.sleep.shorts.entity.TryShorts;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TryShortsRepository extends JpaRepository<TryShorts,Integer> {
 }
