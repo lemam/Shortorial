08eb889dee2a5c0b657d0573a3b5bfb1a9b764ed
dykim <kdayun@daum.net>
2024-04-29 10:16:09 +0900

    Feat: 동영상 소리 삽입
    S10P31B305-291 #in-progress

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -3,7 +3,20 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: "configure-response-headers", // 플러그인 이름 지정
+      configureServer: (server) => {
+        // vite 서버가 시작될 때 호출
+        server.middlewares.use((_req, res, next) => {
+          res.setHeader("Cross-Origin-Embedder-Policy", "require-corp"); // COEP 활성화
+          res.setHeader("Cross-Origin-Opener-Policy", "same-origin"); // COOP 활성화
+          next();
+        });
+      },
+    },
+  ],
   server: {
     port: 3000,
   },
