579b4fe98f91930b740ae86316caf005a1b21f9e
우지민 <0542say@naver.com>
2024-04-30 09:14:35 +0900

    Feat: CorsConfig 추가

---
+++ back/sleep/src/main/java/com/sleep/sleep/common/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.sleep.sleep.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**").allowedOriginPatterns("*")
+                .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name(), HttpMethod.HEAD.name(), HttpMethod.OPTIONS.name(),
+                        HttpMethod.PATCH.name())
+                .exposedHeaders("*")
+                .maxAge(1800);
+    }
+}
