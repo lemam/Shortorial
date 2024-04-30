474e0110a3f62236d27ed3fb6671ca703354e419
우지민 <0542say@naver.com>
2024-04-30 09:22:02 +0900

    Merge file

--- back/sleep/src/main/java/com/sleep/sleep/member/repository/MemberRepository.java
+++ back/sleep/src/main/java/com/sleep/sleep/member/repository/MemberRepository.java
@@ -2,6 +2,8 @@ package com.sleep.sleep.member.repository;
 
 import com.sleep.sleep.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Integer> {
 }
