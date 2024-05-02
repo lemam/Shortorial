48fcf083dead0c829a9fa59668b623a3c555f4f1
임지은 <ljieun01@naver.com>
2024-05-02 13:48:50 +0900

    Design: 전역 CSS에서 html의 width, height의 값을 %에서 vw, vh로 수정

--- front/src/GlobalStyle.tsx
+++ front/src/GlobalStyle.tsx
@@ -5,8 +5,8 @@ const GlobalStyle = createGlobalStyle`
   body,
   #root {
     position: relative;
-    width: 100%;
-    height: 100%;
+    width: 100vw;
+    height: 100vh;
     margin: 0;
     padding: 0;
     box-sizing: border-box;
