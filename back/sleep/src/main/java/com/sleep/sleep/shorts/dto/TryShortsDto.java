6c9dc39e93240cac061fb7c53540473d935cd795
우지민 <0542say@naver.com>
2024-05-10 09:28:27 +0900

    Feat: erd 변경에 따라 갱신

--- back/sleep/src/main/java/com/sleep/sleep/shorts/dto/TryShortsDto.java
+++ back/sleep/src/main/java/com/sleep/sleep/shorts/dto/TryShortsDto.java
@@ -1,4 +1,23 @@
 package com.sleep.sleep.shorts.dto;
 
+import com.sleep.sleep.member.entity.Member;
+import com.sleep.sleep.shorts.entity.Shorts;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TryShortsDto {
+
+    private int tryNo;
+    private int shortsNo;
+    private int memberIndex;
+    private int tryYn;
+
 }
