0891b8deedf1d08ac87a175e1eedd922aa65a47b
우지민 <0542say@naver.com>
2024-05-10 09:28:13 +0900

    Refactor: 의존성 주입을 어노테이션으로 대체

--- back/sleep/src/main/java/com/sleep/sleep/shorts/controller/ShortsController.java
+++ back/sleep/src/main/java/com/sleep/sleep/shorts/controller/ShortsController.java
@@ -3,20 +3,18 @@ package com.sleep.sleep.shorts.controller;
 import com.sleep.sleep.shorts.dto.ShortsDto;
 import com.sleep.sleep.shorts.service.ShortsService;
 import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 public class ShortsController {
 
     private final ShortsService shortsService;
-
-    public ShortsController(ShortsService shortsService) {
-        this.shortsService = shortsService;
-    }
 
     @Operation(summary = "쇼츠 목록 조회")
     @GetMapping("/api/shorts")
