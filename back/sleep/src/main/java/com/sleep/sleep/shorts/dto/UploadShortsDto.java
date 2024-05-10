a8a59acb11c22b0bc84f4498806bd1c6011e11dc
우지민 <0542say@naver.com>
2024-05-10 09:28:44 +0900

    Feat: erd 변경에 따라 갱신

--- back/sleep/src/main/java/com/sleep/sleep/shorts/dto/UploadShortsDto.java
+++ back/sleep/src/main/java/com/sleep/sleep/shorts/dto/UploadShortsDto.java
@@ -1,4 +1,22 @@
 package com.sleep.sleep.shorts.dto;
 
+import com.sleep.sleep.member.entity.Member;
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UploadShortsDto {
+    private int uploadNo;
+    private int memberNo;
+    private String uploadUrl;
+    private String uploadTitle;
+    private String uploadDate;
+
+    public UploadShortsDto( String uploadUrl, String uploadTitle) {
+        this.uploadUrl = uploadUrl;
+        this.uploadTitle = uploadTitle;
+    }
 }
