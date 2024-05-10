e1b84776d97584896a527f6f2c1a4e01b8a1c6d4
이현정 <dlguswjd0226@naver.com>
2024-05-10 17:01:04 +0900

    Feat: 버튼-모션인식 연결
    challenge 페이지만 연결되어있던 모션인식 learn 페이지에도 연결
    연결하면서 생긴 자잘한 이슈 해결
    버튼이 연속으로 눌리지 않도록 수정
    다른 버튼의 위치나 아예 밖에 갔다 와야 버튼 재인식 가능
    
    S10P31B305-543

--- front/src/pages/LearnPageTmp.tsx
+++ front/src/pages/LearnPageTmp.tsx
@@ -1,98 +1,431 @@
+import { useCallback, useEffect, useRef, useState } from "react";
 import styled from "styled-components";
-// import MotionCamera from "../components/motion/MotionCamera";
-import MotionVideo from "../components/motion/MotionVideo";
-// import MotionVideo2 from "../components/motion/MotionVideo copy";
-// import { Acc } from "../modules/Acc";
-import { NormalizedLandmark } from "@mediapipe/tasks-vision";
-import { useState } from "react";
-// import { text } from "stream/consumers";
+import { Flip, Pause, PlayArrow, Repeat, Videocam } from "@mui/icons-material";
+import VideoMotionButton from "../components/button/VideoMotionButton";
+import { VideoSection } from "../constants/types";
+import SectionButtonList from "../components/ButtonList/SectionButtonList";
+import MotionCamera from "../components/motion/MotionCamera";
+import { useBtnStore } from "../store/useMotionStore";
+import { setBtnInfo } from "../modules/Motion";
+import { useNavigate } from "react-router-dom";
 
-export default function LearnPage() {
-  const [, setVideoLandmark] = useState<NormalizedLandmark[] | null>(null);
+const LearnPageTest = () => {
+  type LearnState = "PAUSE" | "READY" | "PLAY";
+  const READY_TIMER = 3;
+  const PLAY_SPEEDS = [1, 0.75, 0.5];
 
-  // const [camLandmark, setCamLandmark] = useState<NormalizedLandmark[] | null>(
-  //   null
-  // );
+  const cameraRef = useRef<HTMLVideoElement>(null);
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const video = videoRef.current;
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
 
-  // const [accValue, setAccValue] = useState(0);
+  const [leftSectionWidth, setLeftSectionWidth] = useState<number>(0);
 
-  const getVideoLandmark = (landmarkData: NormalizedLandmark[]) => {
-    setVideoLandmark(landmarkData);
+  const [state, setState] = useState<LearnState>("PAUSE");
+  const [cameraSize, setCameraSize] = useState({
+    width: 0,
+    height: 0,
+  });
+
+  const [currTimer, setCurrTimer] = useState<number>(READY_TIMER);
+  const [isFlipped, setIsFlipped] = useState<boolean>(false);
+  const [playSpeedIdx, setPlaySpeedIdx] = useState<number>(0);
+  const currPlaySpeed = PLAY_SPEEDS[playSpeedIdx];
+  const [isLooping, setISLooping] = useState<boolean>(false);
+
+  const [sectionList, setSectionList] = useState<VideoSection[]>([]);
+  const [currentTime, setCurrentTime] = useState<number>(0);
+
+  const navigate = useNavigate();
+
+  // 카메라 설정 초기화
+  const initCamera = useCallback(() => {
+    // 미디어 설정
+    const constraints: MediaStreamConstraints = {
+      video: {
+        aspectRatio: 9 / 16,
+        facingMode: "user", // 전면 카메라 사용
+      },
+      audio: false,
+    };
+
+    // 카메라 권한 요청
+    navigator.mediaDevices
+      .getUserMedia(constraints)
+      .then((stream: MediaStream) => {
+        if (cameraRef.current) cameraRef.current.srcObject = stream;
+      })
+      .catch((error: Error) => {
+        alert("카메라 권한을 찾을 수 없습니다.");
+        console.error("Media device access error:", error);
+      });
+  }, []);
+
+  // 카메라 크기 초기화
+  const initVideoSize = () => {
+    let height = 0;
+    let width = 0;
+
+    // 모바일 세로인 경우
+    if (window.innerWidth < 480) {
+      height = window.innerHeight * 0.8;
+      width = Math.floor((height * 9) / 16);
+    }
+    // 모바일 가로 또는 가로 길이 480 이상인 경우
+    else {
+      height = window.innerHeight - 1;
+      width = Math.floor((height * 9) / 16);
+    }
+
+    setCameraSize({ width, height });
   };
 
-  // const getCamLandmark = (landmarkData: NormalizedLandmark[]) => {
-  //   setCamLandmark(landmarkData);
-  // };
+  // LeftSection 요소의 너비 저장
+  const measuredRef = useCallback((node: HTMLElement | null) => {
+    if (node) {
+      setLeftSectionWidth(node.getBoundingClientRect().width);
+    } else {
+      setLeftSectionWidth(0);
+    }
+  }, []);
 
-  // const accValue =
-  //   videoLandmark && camLandmark ? Acc(videoLandmark, camLandmark) : 0;
+  // 타이머 초기화
+  const initInterval = useCallback(() => {
+    if (intervalRef.current) clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setCurrTimer(READY_TIMER);
+  }, [READY_TIMER]);
 
-  // useEffect(() => {
-  //   if (videoLandmark && camLandmark) {
-  //     const accValue = Acc(videoLandmark, camLandmark);
-  //     setAccValue(accValue);
-  //     // console.log(accValue);
-  //   }
-  // }, [videoLandmark, camLandmark]);
+  // PAUSE 상태로 변경
+  const changeStatePause = useCallback(() => {
+    // 타이머 중지한다.
+    // 동영상 중지 후 처음으로 돌아간다.
+    if (video) {
+      initInterval();
+      video.pause();
+    }
+    setState("PAUSE");
+  }, [initInterval, video]);
+
+  // READY 상태로 변경
+  const changeStateReady = () => {
+    // 타이머 1초씩 카운트다운
+    intervalRef.current = setInterval(
+      () => setCurrTimer((prev) => prev - 1),
+      1000
+    );
+    setState("READY");
+  };
+
+  // 동영상 재생 속도 버튼 클릭 이벤트 리스너
+  const handlePlaySpeedButtonClick = () => {
+    const nextIdx = (playSpeedIdx + 1) % PLAY_SPEEDS.length;
+    setPlaySpeedIdx(nextIdx);
+  };
+
+  // PLAY 상태로 변경
+  const changeStatePlay = useCallback(() => {
+    // 동영상 재생
+    if (video) {
+      video.playbackRate = currPlaySpeed;
+      video.play();
+    }
+    setState("PLAY");
+  }, [currPlaySpeed, video]);
+
+  // 동영상 구간 리스트 반환
+  const getSectionList = () => {
+    // 동영상을 3초마다 구간으로 나눈다.
+    // TODO: API로 받을 값 (동영상 전체 길이)
+    const videoLength = 17;
+    const secondsPerSection = 3;
+    const numberOfSections = videoLength / secondsPerSection;
+    const result: VideoSection[] = [];
+
+    for (let i = 0; i < numberOfSections; i++) {
+      result.push({
+        id: i,
+        start: i * secondsPerSection,
+        end: (i + 1) * secondsPerSection,
+      });
+    }
+
+    return result;
+  };
+
+  // 비디오 시간 time초로 이동
+  const moveVideoTime = (time: number) => {
+    if (video) {
+      video.currentTime = time;
+      setCurrentTime(time);
+    }
+  };
+
+  // 카메라 초기화
+  useEffect(() => {
+    initCamera();
+    initVideoSize();
+
+    window.addEventListener("resize", initVideoSize);
+
+    return () => {
+      window.removeEventListener("resize", initVideoSize);
+    };
+  }, [initCamera]);
+
+  // 타이머 감시
+  useEffect(() => {
+    // 카운트다운이 끝나면 재생 상태로 변경
+    if (currTimer <= 0) {
+      initInterval();
+      changeStatePlay();
+    }
+  }, [changeStatePlay, currTimer, initInterval]);
+
+  // 동영상 감시
+  const { btn, setBtn } = useBtnStore();
+  useEffect(() => {
+    if (video) {
+      video.addEventListener("ended", changeStatePause);
+      video.addEventListener("timeupdate", () =>
+        setCurrentTime(video.currentTime)
+      );
+    }
+
+    return () => {
+      if (video) {
+        video.removeEventListener("ended", changeStatePause);
+        video.removeEventListener("timeupdate", () =>
+          setCurrentTime(video.currentTime)
+        );
+      }
+    };
+  }, [changeStatePause, video]);
+
+  // 동영상 구간 리스트 가져오기
+  useEffect(() => {
+    setSectionList(getSectionList());
+  }, []);
+
+  useEffect(() => {
+    switch (btn) {
+      case "play":
+        if (state == "PAUSE") changeStateReady();
+        else changeStatePause();
+        break;
+      case "challenge":
+        navigate("/challenge");
+        break;
+      case "repeat":
+        setISLooping(!isLooping);
+        break;
+      case "flip":
+        setIsFlipped(!isFlipped);
+        break;
+      case "speed":
+        handlePlaySpeedButtonClick();
+        break;
+    }
+  }, [btn]);
+
+  useEffect(() => {
+    setBtnInfo();
+  }, [cameraSize.width]);
   return (
     <Container>
-      <VideoContainer>
-        <MotionVideo width={500} height={700} getLandmark={getVideoLandmark} />
-      </VideoContainer>
-      {/* <VideoContainer>
-        <MotionVideo2 width={500} height={700} getLandmark={getCamLandmark} />
-      </VideoContainer> */}
-      <MotionCameraContainer>
-        {/* <MotionCamera width={500} height={700} /> */}
-      </MotionCameraContainer>
-      {/* <div id="Acc" style={{ background: "white", width: "100%" }}>
-        Acc: {accValue}
-      </div> */}
+      <LeftSection ref={(el) => measuredRef(el)}>
+        <SectionButtonList
+          sectionList={sectionList}
+          parentWidth={leftSectionWidth}
+          currentTime={currentTime}
+          clickHandler={(section) => moveVideoTime(section.start)}
+        />
+      </LeftSection>
+      <CenterSection>
+        <VideoContainer>
+          <video
+            width={cameraSize.width}
+            height={cameraSize.height}
+            src="src/assets/sample.mp4"
+            ref={videoRef}
+            className={isFlipped ? "flip" : ""}
+            controls
+          ></video>
+        </VideoContainer>
+        <VideoContainer id="dom">
+          <MotionCamera
+            width={cameraSize.width}
+            height={cameraSize.height}
+            className="camera flip"
+            autoPlay
+          ></MotionCamera>
+          <VideoMotionButtonList>
+            {state === "PAUSE" ? (
+              <VideoMotionButton
+                id="play"
+                icon={<PlayArrow />}
+                toolTip="재생"
+                onClick={changeStateReady}
+              />
+            ) : (
+              <VideoMotionButton
+                id="play"
+                icon={<Pause />}
+                toolTip="일시정지"
+                onClick={changeStatePause}
+              />
+            )}
+            <div className="foldList">
+              <VideoMotionButton
+                id="challenge"
+                icon={<Videocam />}
+                toolTip="챌린지 모드로 이동"
+                link="/challenge"
+              />
+              {isLooping ? (
+                <VideoMotionButton
+                  id="repeat"
+                  icon={<Repeat />}
+                  toolTip="구간 반복 해제"
+                  onClick={() => setISLooping(!isLooping)}
+                />
+              ) : (
+                <VideoMotionButton
+                  id="repeat"
+                  imgSrc="src/assets/icon/repeat-off.svg"
+                  toolTip="구간 반복"
+                  onClick={() => setISLooping(!isLooping)}
+                />
+              )}
+              <VideoMotionButton
+                id="flip"
+                icon={<Flip />}
+                toolTip="거울 모드"
+                onClick={() => setIsFlipped(!isFlipped)}
+              />
+              <VideoMotionButton
+                id="speed"
+                text={`${currPlaySpeed}x`}
+                toolTip="재생 속도"
+                onClick={handlePlaySpeedButtonClick}
+              />
+            </div>
+          </VideoMotionButtonList>
+        </VideoContainer>
+        {state === "READY" && <Timer>{currTimer}</Timer>}
+      </CenterSection>
+      <RightSection></RightSection>
     </Container>
   );
-}
+};
 
 const Container = styled.div`
   position: relative;
-  display: flex;
   width: 100%;
   height: 100%;
+  display: flex;
   background-color: #000;
 
-  @media screen and (orientation: portrait) {
-    flex-direction: column;
+  @media screen and (max-width: 479px) {
+    display: flex;
+    flex-direction: column-reverse;
   }
+`;
 
-  @media screen and (orientation: landscape) {
-    flex-direction: row;
-    justify-content: center;
+const Section = styled.section`
+  position: relative;
+  display: flex;
+`;
+
+const RightSection = styled(Section)`
+  flex: 1;
+  margin: 8px;
+
+  @media screen and (max-width: 479px) {
+    display: none;
+  }
+`;
+
+const CenterSection = styled(Section)`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
+
+const LeftSection = styled(Section)`
+  flex: 1;
+  justify-content: flex-end;
+  align-items: center;
+  margin: 8px;
+  color: #fff;
+
+  @media screen and (max-width: 479px) {
+    align-items: start;
   }
 `;
 
 const VideoContainer = styled.div`
-  @media screen and (orientation: portrait) {
-    width: 100%;
-    height: 80%;
-    display: flex;
-    justify-content: center;
-  }
-
-  @media screen and (orientation: landscape) {
-    height: 100%;
-  }
-`;
-
-const MotionCameraContainer = styled.div`
-  position: relative;
-  width: 100%;
+  display: flex;
   height: 100%;
 
-  @media screen and (orientation: portrait) {
-    display: block;
+  video {
+    display: flex;
+    object-fit: cover;
   }
 
-  @media screen and (orientation: landscape) {
-    display: block;
-    transform: scaleX(-1); // 수평으로 뒤집기
+  video.flip {
+    transform: scaleX(-1);
+  }
+
+  @media screen and (max-width: 479px) {
+    .camera {
+      display: none;
+    }
   }
 `;
+
+const Timer = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 84px;
+  height: 84px;
+  font-size: 48px;
+  color: #fff;
+  background: #35353580;
+  border: 5px solid #fff;
+  border-radius: 50%;
+`;
+
+const VideoMotionButtonList = styled.div`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  margin: 8px;
+
+  .foldList {
+    display: flex;
+    flex-direction: column;
+  }
+
+  button {
+    display: inline-block;
+    margin-bottom: 24px;
+  }
+
+  @media screen and (min-width: 768px) {
+    button {
+      width: 52px;
+      height: 52px;
+      margin-bottom: 36px;
+    }
+  }
+`;
+
+export default LearnPageTest;
--- front/src/store/useMotionStore.tsx
+++ front/src/store/useMotionStore.tsx
@@ -7,21 +7,31 @@ interface BtnState {
 
 interface DomState {
   domSize: DOMRect | null;
-  playSize: DOMRect | null;
   visibleBtnSize: DOMRect | null;
   timerBtnSize: DOMRect | null;
   recordBtnSize: DOMRect | null;
   saveBtnSize: DOMRect | null;
-  modeBtnSize: DOMRect | null;
+  learnBtnSize: DOMRect | null;
   rsltBtnSize: DOMRect | null;
   setDomSize: (newDomSize: DOMRect) => void;
-  setPlaySize: (newDomSize: DOMRect) => void;
   setVisibleBtnSize: (newBtnSize: DOMRect) => void;
   setTimeBtnSize: (newBtnSize: DOMRect) => void;
   setRecordBtnSize: (newBtnSize: DOMRect) => void;
   setSaveBtnSize: (newBtnSize: DOMRect) => void;
-  setModeBtnSize: (newBtnSize: DOMRect) => void;
+  setLearnBtnSize: (newBtnSize: DOMRect) => void;
   setRsltBtnSize: (newBtnSize: DOMRect) => void;
+
+  playSize: DOMRect | null;
+  challengeSize: DOMRect | null;
+  repeatSize: DOMRect | null;
+  flipSize: DOMRect | null;
+  speedSize: DOMRect | null;
+
+  setPlaySize: (newDomSize: DOMRect) => void;
+  setChallengeSize: (newDomSize: DOMRect) => void;
+  setRepeatSize: (newDomSize: DOMRect) => void;
+  setFlipSize: (newDomSize: DOMRect) => void;
+  setSpeedSize: (newDomSize: DOMRect) => void;
 }
 
 export const useBtnStore = create<BtnState>((set) => ({
@@ -31,21 +41,31 @@ export const useBtnStore = create<BtnState>((set) => ({
 
 export const useDomStore = create<DomState>((set) => ({
   domSize: null,
-  playSize: null,
   visibleBtnSize: null,
   timerBtnSize: null,
   recordBtnSize: null,
   saveBtnSize: null,
-  modeBtnSize: null,
+  learnBtnSize: null,
   rsltBtnSize: null,
 
+  playSize: null,
+  challengeSize: null,
+  repeatSize: null,
+  flipSize: null,
+  speedSize: null,
+
   setDomSize: (newDomSize: DOMRect) => set({ domSize: newDomSize }),
-  setPlaySize: (newDomSize: DOMRect) => set({ playSize: newDomSize }),
   setVisibleBtnSize: (newBtnSize: DOMRect) =>
     set({ visibleBtnSize: newBtnSize }),
   setTimeBtnSize: (newBtnSize: DOMRect) => set({ timerBtnSize: newBtnSize }),
   setRecordBtnSize: (newBtnSize: DOMRect) => set({ recordBtnSize: newBtnSize }),
   setSaveBtnSize: (newBtnSize: DOMRect) => set({ saveBtnSize: newBtnSize }),
-  setModeBtnSize: (newBtnSize: DOMRect) => set({ modeBtnSize: newBtnSize }),
+  setLearnBtnSize: (newBtnSize: DOMRect) => set({ learnBtnSize: newBtnSize }),
   setRsltBtnSize: (newBtnSize: DOMRect) => set({ rsltBtnSize: newBtnSize }),
+
+  setPlaySize: (newDomSize: DOMRect) => set({ playSize: newDomSize }),
+  setChallengeSize: (newDomSize: DOMRect) => set({ challengeSize: newDomSize }),
+  setRepeatSize: (newDomSize: DOMRect) => set({ repeatSize: newDomSize }),
+  setFlipSize: (newDomSize: DOMRect) => set({ flipSize: newDomSize }),
+  setSpeedSize: (newDomSize: DOMRect) => set({ speedSize: newDomSize }),
 }));
