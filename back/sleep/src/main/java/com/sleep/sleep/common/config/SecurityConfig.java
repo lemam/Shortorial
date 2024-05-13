06ffd16bd9ded7bde6bc573b70f66b8da612cdc1
전성수 <ke_wer@naver.com>
2024-05-13 14:07:24 +0900

    Fix: lambda 에서 method 방식으로 변경

--- back/sleep/src/main/java/com/sleep/sleep/common/config/SecurityConfig.java
+++ back/sleep/src/main/java/com/sleep/sleep/common/config/SecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -38,10 +40,9 @@ public class SecurityConfig {
                         auth.requestMatchers("/").hasRole("USER")
                         .anyRequest().permitAll())
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .csrf((csrfConfig)->csrfConfig.disable())
-                .headers((headerConfig)->headerConfig.frameOptions(frameOptionsConfig ->
-                        frameOptionsConfig.disable()))
-                .logout((logoutConfig)->logoutConfig.disable())
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers((headerConfig)->headerConfig.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .logout(AbstractHttpConfigurer::disable)
                 //jwt로 로그인 로그아웃 처리할 것이므로 disable
                 .sessionManagement((sessionManagement)->sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 //기본적으로 session을 이용하게 되는데 redis를 이용할 것이니까 STATELESS
