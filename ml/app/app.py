bea8a3338a9dfca41437f2ff1735f02e3a9a052d
이현정 <dlguswjd0226@naver.com>
2024-05-14 09:08:13 +0900

    Init: 프로젝트 구조 초기화
    깡통함수만 만들고 구조 잡기
    
    S10P31B305-588

--- ml/app/app.py
+++ ml/app/app.py
@@ -1,10 +1,22 @@
 import os
 from dotenv import load_dotenv
 from flask import Flask
-
+from ..models.recommend_music import get_recommend_music, train_model
 
 def create_app():
-    app = Flask(__name__)    
+    app = Flask(__name__)
+    
+    # 노래 추천 결과 받기
+    @app.route("/pyapi/music/<int:userId>", method=["GET"])    
+    def recommend_music(userId):
+        music_list = get_recommend_music(userId)
+        return music_list
+    
+    # 학습
+    @app.route("pyapy/music/train/<int:musicId>", method=["POST"])
+    def train(musicId, userId):
+        train_model(userId)
+        
     return app
 
 if __name__ == "__main__":
