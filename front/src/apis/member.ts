4ff38ca016a9962c88d5d2532bc050a249eebba4
우지민 <0542say@naver.com>
2024-05-15 00:21:28 +0900

    Feat: interface 및 memberInfo 추가

--- front/src/apis/member.ts
+++ front/src/apis/member.ts
@@ -2,14 +2,18 @@ import { axios } from "../utils/axios";
 
 const REST_MEMBER_URL = "/api/member";
 
+export interface MemberInfo {
+  username: String;
+  memberNickname: String;
+  memberProfile: String;
+}
+
 // 중복검사
 export function getDuplicateCheck(cate: string, input: string) {
   try {
-    const response = axios
-      .get(REST_MEMBER_URL + `/check/${cate}/${input}`)
-      .then((res) => {
-        return res.data.dupCheck;
-      });
+    const response = axios.get(REST_MEMBER_URL + `/check/${cate}/${input}`).then((res) => {
+      return res.data.dupCheck;
+    });
     return response;
   } catch (error) {
     console.error("Error fetching data:", error);
@@ -31,6 +35,23 @@ export function signUp(id: string, pass: string, nickname: string) {
         return res.data;
       });
     return response;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+// 회원정보
+export async function getInfo() {
+  try {
+    const token = "Bearer " + localStorage.getItem("accessToken");
+
+    const response = await axios.get(`${REST_MEMBER_URL}/info`, {
+      headers: {
+        Authorization: token,
+      },
+    });
+
+    return response.data;
   } catch (error) {
     console.error("Error fetching data:", error);
   }
