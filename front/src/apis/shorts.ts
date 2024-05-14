a57e9f68a1e22b1557c3470ed3c7504ae3cd1f5a
우지민 <0542say@naver.com>
2024-05-14 10:49:13 +0900

    Merge branch 'develop' of https://lab.ssafy.com/s10-final/S10P31B305 into feature/fe/mypage

--- front/src/apis/shorts.ts
+++ front/src/apis/shorts.ts
@@ -4,8 +4,34 @@ const REST_SHORTS_URL = "api/s3/upload";
 const REST_SHORTS_LIST_URL = "/api/shorts";
 const REST_MYPAGE_URL = "/api/mypage";
 
+export interface shorts {
+  shortsNo: number;
+  shortsUrl: string;
+  shortsTitle: string;
+  shortsDirector: string;
+  shortsTime: number;
+  shortsChallengers: number;
+  shortsLink: string;
+  shortsDate: string;
+}
+
+export interface UploadShorts {
+  uploadNo: number;
+  memberNo: number;
+  uploadUrl: string;
+  uploadTitle: string;
+  uploadDate: string;
+}
+
+export interface TryShorts {
+  tryNo: number;
+  shortsNo: number;
+  memberNo: number;
+  tryYn: number;
+}
+
 // S3 동영상 업로드
-export async function postUploadShorts(blob: blob, fileName: string) {
+export async function postUploadShorts(blob: Blob, fileName: string) {
   try {
     const token = "Bearer " + localStorage.getItem("accessToken");
 
@@ -25,17 +51,6 @@ export async function postUploadShorts(blob: blob, fileName: string) {
   } catch (error) {
     console.error("Error fetching data:", error);
   }
-}
-
-export interface shorts {
-  shortsNo: number;
-  shortsUrl: string;
-  shortsTitle: string;
-  shortsDirector: string;
-  shortsTime: number;
-  shortsChallengers: number;
-  shortsLink: string;
-  shortsDate: string;
 }
 
 // 쇼츠 리스트 조회
@@ -63,14 +78,6 @@ export const getShortsInfo = async (shortsNo: number) => {
     console.error(error);
   }
 };
-
-export interface UploadShorts {
-  uploadNo: number;
-  memberNo: number;
-  uploadUrl: string;
-  uploadTitle: string;
-  uploadDate: string;
-}
 
 //사용자가 저장한 쇼츠 조회
 export async function getUploadedShorts() {
@@ -127,6 +134,23 @@ export async function getTryCount(shortsNo: number) {
     });
 
     return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+//사용자가 시도한 쇼츠 조회
+export async function getTryShorts() {
+  try {
+    const token = "Bearer " + localStorage.getItem("accessToken");
+
+    const data = await axios.get(`${REST_MYPAGE_URL}/try-shorts`, {
+      headers: {
+        Authorization: token,
+      },
+    });
+
+    return data.data;
   } catch (error) {
     console.error("Error fetching data:", error);
   }
--- front/src/components/mypage/TryList.tsx
+++ front/src/components/mypage/TryList.tsx
@@ -1,10 +1,37 @@
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+
+import { TryShorts, getTryShorts } from "../../apis/shorts";
+import ChallengeResultPage from "./ChallengeComponent";
 
 export default function TryList() {
+  const [shortsList, setShortsList] = useState<TryShorts[]>([]);
+
+  const getShorts = async () => {
+    try {
+      const data = await getTryShorts();
+      setShortsList(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    getShorts();
+  }, []);
+
   return (
     <div>
-      <h1>TryList 부분</h1>
-      <p>이런</p>
+      {/* {Array.from({ length: Math.ceil(shortsList.length / 4) }, (_, index) => index * 4).map(
+        (startIndex) => (
+          <div key={startIndex}>
+            <div style={{ display: "flex" }}>
+              {shortsList.slice(startIndex, startIndex + 4).map((uploadShorts) => (
+                <ChallengeResultPage uploadShorts={uploadShorts} />
+              ))}
+            </div>
+          </div>
+        )
+      )} */}
     </div>
   );
 }
