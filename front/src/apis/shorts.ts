0abdae61134c63c0afc08d271a243ea727c3fdcd
우지민 <0542say@naver.com>
2024-05-14 21:59:06 +0900

    Rename: mypage 분리

--- front/src/apis/shorts.ts
+++ front/src/apis/shorts.ts
@@ -2,7 +2,6 @@ import { axios } from "../utils/axios";
 
 const REST_SHORTS_URL = "api/s3/upload";
 const REST_SHORTS_LIST_URL = "/api/shorts";
-const REST_MYPAGE_URL = "/api/mypage";
 
 export interface shorts {
   shortsNo: number;
@@ -73,46 +72,6 @@ export const getShortsInfo = async (shortsNo: number) => {
   }
 };
 
-//사용자가 저장한 쇼츠 조회
-export async function getUploadedShorts() {
-  try {
-    const token = "Bearer " + localStorage.getItem("accessToken");
-
-    const data = await axios.get(`${REST_MYPAGE_URL}/upload-shorts`, {
-      headers: {
-        Authorization: token,
-      },
-    });
-
-    return data.data;
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  }
-}
-
-//사용자가 저장한 쇼츠 이름 수정
-export async function putUpdateTitle(oldTitle: string, newTitle: string, uploadNo: number) {
-  try {
-    const token = "Bearer " + localStorage.getItem("accessToken");
-
-    const data = {
-      uploadNo: uploadNo,
-      oldTitle: oldTitle,
-      newTitle: newTitle,
-    };
-
-    const response = await axios.put(`${REST_MYPAGE_URL}/rename`, data, {
-      headers: {
-        Authorization: token,
-      },
-    });
-
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  }
-}
-
 //영상 시도하면 카운트
 export async function getTryCount(shortsNo: number) {
   try {
@@ -128,23 +87,6 @@ export async function getTryCount(shortsNo: number) {
     });
 
     return response.data;
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  }
-}
-
-//사용자가 시도한 쇼츠 조회
-export async function getTryShorts() {
-  try {
-    const token = "Bearer " + localStorage.getItem("accessToken");
-
-    const data = await axios.get(`${REST_MYPAGE_URL}/try-shorts`, {
-      headers: {
-        Authorization: token,
-      },
-    });
-
-    return data.data;
   } catch (error) {
     console.error("Error fetching data:", error);
   }
