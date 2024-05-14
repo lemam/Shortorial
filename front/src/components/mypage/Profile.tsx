ca9888e8b4a3d77234d80c358cf69bde5f823da1
우지민 <0542say@naver.com>
2024-05-15 00:22:11 +0900

    Feat: profile 컴포넌트 정보 추가

--- front/src/components/mypage/Profile.tsx
+++ front/src/components/mypage/Profile.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { Countings, getCounting } from "../../apis/mypage";
+import { MemberInfo, getInfo } from "../../apis/member";
 
 export default function Profile() {
   const [counting, setCounting] = useState<Countings>();
-
+  const [memberInfo, setMemberInfo] = useState<MemberInfo>();
+  let imageUrl = "/src/assets/profiles/profile";
   const getCounts = async () => {
     try {
       const data = await getCounting();
@@ -14,40 +16,94 @@ export default function Profile() {
     }
   };
 
+  const getMemberInfo = async () => {
+    try {
+      const data = await getInfo();
+      setMemberInfo(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
   useEffect(() => {
     getCounts();
+    getMemberInfo();
+    if (memberInfo) {
+      imageUrl = `${imageUrl}${memberInfo.memberProfile}.jpeg`;
+    }
   }, []);
+
   return (
     <ProfileContainer>
-      <ProfileInfoContainer>Profile 지역</ProfileInfoContainer>
-      <ProfileNumContainer>
+      <ProfileInfoContainer>
         <div>
+          <ProfileImage
+            src={`/src/assets/profiles/profile${memberInfo?.memberProfile}.jpeg`}
+            alt="프로필 이미지"
+          />
+        </div>
+        <div>{memberInfo?.memberNickname}</div>
+      </ProfileInfoContainer>
+
+      <ProfileNumContainer>
+        <div
+          key="tryShorts"
+          className="innerDiv"
+        >
           <div>{counting?.tryShortsCount}</div>
           <div>참여</div>
         </div>
-        <div>
+        <div
+          key="uploadShorts"
+          className="innerDiv"
+        >
           <div>{counting?.uploadShortsCount}</div>
-          <div>저장</div>
+          <div>완료</div>
         </div>
-        <div>
+        <div
+          key="youtubeUrl"
+          className="innerDiv"
+        >
           <div>{counting?.youtubeUrlCount}</div>
-          <div>업로드</div>
+          <div>게시</div>
         </div>
       </ProfileNumContainer>
     </ProfileContainer>
   );
 }
+
 const ProfileContainer = styled.div`
   display: flex;
   height: 100%;
-  border: 1px solid black;
   flex-direction: column; /* 세로로 나열 */
+  border: 1px solid black;
+  padding: 10px;
 `;
 const ProfileInfoContainer = styled.div`
   display: flex;
-  border: 1px solid black;
+  justify-content: center; /* 수평 가운데 정렬 */
+  align-items: center; /* 수직 가운데 정렬 */
+  padding: 10px;
+`;
+const ProfileImage = styled.img`
+  height: 50px;
+  padding-right: 50px;
 `;
 const ProfileNumContainer = styled.div`
   display: flex;
-  border: 1px solid black;
+  justify-content: center;
+  border-top: 1px solid black;
+  padding: 10px;
+
+  .innerDiv {
+    display: flex;
+    flex-direction: column; /* 세로로 나열 */
+    justify-content: center; /* 수평 가운데 정렬 */
+    align-items: center; /* 수직 가운데 정렬 */
+
+    width: 20vw;
+
+    margin-right: 10px;
+    margin-left: 10px;
+  }
 `;
