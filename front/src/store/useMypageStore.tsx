9db4f00dfec76b5c52f613c39e5be667d2c5ff82
우지민 <0542say@naver.com>
2024-05-17 10:44:27 +0900

    Feat: MypageStore 인터페이스 추가

--- front/src/store/useMypageStore.tsx
+++ front/src/store/useMypageStore.tsx
@@ -1,13 +1,19 @@
 import { create } from "zustand";
 import { UploadShorts, shorts } from "../apis/shorts";
 
-const useMypageStore = create((set) => ({
+interface MyPageStore {
+  uploadedShorts: UploadShorts[];
+  tryShorts: shorts[];
+  setUploadedShorts: (data: UploadShorts[]) => void;
+  setTryShorts: (data: shorts[]) => void;
+}
+
+const useMypageStore = create<MyPageStore>((set) => ({
   uploadedShorts: [], // 업로드한 동영상 리스트
   tryShorts: [], // 시도한 영상 리스트
   // 업로드한 동영상 리스트 업데이트
-  setUploadedShorts: (UploadShorts: UploadShorts) => set({ uploadedShorts: UploadShorts }),
+  setUploadedShorts: (data: UploadShorts[]) => set({ uploadedShorts: data }),
   // 시도한 영상 리스트 업데이트
-  setTryShorts: (shorts: shorts) => set({ tryShorts: shorts }),
+  setTryShorts: (data: shorts[]) => set({ tryShorts: data }),
 }));
-
 export default useMypageStore;
