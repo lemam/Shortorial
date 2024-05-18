a5f6295c8de71dd5b30cf233184b25ee28986d4a
dykim <kdayun@daum.net>
2024-05-18 14:09:00 +0900

    Fix: 상세 페이지 비율 조정

--- front/src/pages/ShortsDetailPage.tsx
+++ front/src/pages/ShortsDetailPage.tsx
@@ -5,6 +5,7 @@ import styled from "styled-components";
 import { useNavigate, useParams } from "react-router-dom";
 import { Button } from "react-bootstrap";
 import { getTryCount } from "../apis/shorts";
+import Header from "../components/header/Header";
 
 const ShortsDetailPage = () => {
   const navigate = useNavigate();
@@ -42,44 +43,32 @@ const ShortsDetailPage = () => {
 
   return (
     <Container>
-      <Header>Let's DANCE</Header>
+      <Header></Header>
       <ShortsDetailContainer>
-        <VideoBox>
-          <video
-            src={shortsInfo?.shortsLink}
-            crossOrigin="anonymous"
-            autoPlay
-            loop
-            controls
-          />
-        </VideoBox>
-        <DetailItem>
-          <Label>제목</Label>
-          <Value>{shortsInfo?.shortsTitle}</Value>
-        </DetailItem>
-        <DetailItem>
-          <Label>챌린저</Label>
-          <Value>{shortsInfo?.shortsDirector}</Value>
-        </DetailItem>
-        <DetailItem>
-          <Label>시간</Label>
-          <Value>{shortsInfo?.shortsTime}</Value>
-        </DetailItem>
-        <DetailItem>
-          <Label>참여 인원</Label>
-          <Value>{shortsInfo?.shortsChallengers}</Value>
-        </DetailItem>
+        <VideoBox src={shortsInfo?.shortsLink} crossOrigin="anonymous"></VideoBox>
+        <DetailContainer>
+          <DetailItem>
+            <Label>제목</Label>
+            <Value>{shortsInfo?.shortsTitle}</Value>
+          </DetailItem>
+          <DetailItem>
+            <Label>챌린저</Label>
+            <Value>{shortsInfo?.shortsDirector}</Value>
+          </DetailItem>
+          <DetailItem>
+            <Label>시간</Label>
+            <Value>{shortsInfo?.shortsTime}</Value>
+          </DetailItem>
+          <DetailItem>
+            <Label>참여 인원</Label>
+            <Value>{shortsInfo?.shortsChallengers}</Value>
+          </DetailItem>
+        </DetailContainer>
         <ButtonList>
-          <Button
-            variant="secondary"
-            onClick={goToLearnMode}
-          >
+          <Button variant="secondary" onClick={goToLearnMode}>
             연습 모드
           </Button>
-          <Button
-            variant="primary"
-            onClick={goToChallengeMode}
-          >
+          <Button variant="primary" onClick={goToChallengeMode}>
             챌린지 모드
           </Button>
         </ButtonList>
@@ -90,86 +79,33 @@ const ShortsDetailPage = () => {
 
 export default ShortsDetailPage;
 
-// Component 로 나중에 빼자
-const Header = styled.header`
-  width: 100%;
-  background-color: #f8f9fa;
-  padding: 10px 20px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-weight: bold;
-  margin-bottom: 10px;
-`;
-
 const Container = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  //   & > * {
-  //     margin-bottom: 50px;
-  //   }
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
 `;
 const ShortsDetailContainer = styled.div`
-  @media (orientation: landscape) {
-  }
-`;
-
-const VideoBox = styled.div`
-  position: relative;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
   width: 100%;
-  max-width: 286px;
-  border-radius: 8px;
-  overflow: hidden;
-  margin-bottom: 10px;
-
-  video {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
-
-  @media (orientation: landscape) {
-    video {
-      width: 60%;
-      height: 60%;
-      object-fit: cover;
-    }
-  }
+  height: 100%;
 `;
 
-// // 쇼츠 제목
-// const VideoTitle = styled.div`
-//   font-weight: bold;
-//   white-space: nowrap;
-//   overflow: hidden;
-//   text-overflow: ellipsis;
-// //   margin-bottom: 10px;
-// `;
+const VideoBox = styled.video`
+  height: 70%;
+  width: 100%;
+`;
 
-// const ButtonList = styled.div`
-//   button{
-//     margin: 0 10px;
-//   }
-// `
+const DetailContainer = styled.div`
+  height: 30%;
+  width: 100%;
+`;
 
 const DetailItem = styled.div`
   display: flex;
   align-items: center;
   justify-content: start;
-  width: 100%;
-  padding: 2px 40px;
-  //   background-color: #f4f4f4;
-  //   border-bottom: 1px solid #ddd;
-  font-size: 16px;
-
-  &:last-child {
-    border-bottom: none;
-  }
 `;
 
 const Label = styled.span`
