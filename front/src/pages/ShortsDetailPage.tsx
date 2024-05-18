92a0a39708847ca98fa6e1918344db3d660f34b1
dykim <kdayun@daum.net>
2024-05-18 17:12:35 +0900

    Feat: 영상 상세 띄우기

--- front/src/pages/ShortsDetailPage.tsx
+++ front/src/pages/ShortsDetailPage.tsx
@@ -1,78 +1,82 @@
 import { useEffect, useState } from "react";
 import { Shorts } from "../constants/types";
-import { axios } from "../utils/axios";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { useNavigate, useParams } from "react-router-dom";
-import { Button } from "react-bootstrap";
-import { getTryCount } from "../apis/shorts";
+import { getTryCount, getShortsInfo } from "../apis/shorts";
 import Header from "../components/header/Header";
+import { CancelPresentation, EmojiPeople, MusicNote, TimerOutlined } from "@mui/icons-material";
 
 const ShortsDetailPage = () => {
   const navigate = useNavigate();
+  const params = useParams();
+  const [shortsNo, setShortsNo] = useState<string>("");
+  const [shortsInfo, setShortsInfo] = useState<Shorts>();
+  const [showDetails, setShowDetails] = useState<boolean>(false);
+
+  const getShorts = async (shortsNo: string) => {
+    const shorts = await getShortsInfo(shortsNo);
+    setShortsInfo(shorts);
+  };
 
   const goToLearnMode = () => {
-    const shortsNo = parseInt(params.shortsNo || "", 10);
-    if (!isNaN(shortsNo)) {
-      getTryCount(shortsNo);
-      navigate(`/learn/${shortsNo}`);
-    }
+    getTryCount(shortsNo);
+    navigate(`/learn/${shortsNo}`);
   };
 
   const goToChallengeMode = () => {
-    const shortsNo = parseInt(params.shortsNo || "", 10);
-    if (!isNaN(shortsNo)) {
-      getTryCount(shortsNo);
-      navigate(`/challenge/${shortsNo}`);
-    }
+    getTryCount(shortsNo);
+    navigate(`/challenge/${shortsNo}`);
   };
 
-  const params = useParams();
-
-  const [shortsInfo, setShortsInfo] = useState<Shorts>();
+  const handleVideoClick = () => {
+    setShowDetails(!showDetails);
+  };
 
   useEffect(() => {
-    axios
-      .get(`/api/shorts/${params.shortsNo}`)
-      .then((response) => {
-        setShortsInfo(response.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
+    if (params.shortsNo) {
+      setShortsNo(params.shortsNo);
+      getShorts(params.shortsNo);
+    }
   }, []);
 
   return (
     <Container>
       <Header></Header>
-      <ShortsDetailContainer>
-        <VideoBox src={shortsInfo?.shortsLink} crossOrigin="anonymous"></VideoBox>
-        <DetailContainer>
-          <DetailItem>
-            <Label>제목</Label>
-            <Value>{shortsInfo?.shortsTitle}</Value>
-          </DetailItem>
-          <DetailItem>
-            <Label>챌린저</Label>
-            <Value>{shortsInfo?.shortsDirector}</Value>
-          </DetailItem>
-          <DetailItem>
-            <Label>시간</Label>
-            <Value>{shortsInfo?.shortsTime}</Value>
-          </DetailItem>
-          <DetailItem>
-            <Label>참여 인원</Label>
-            <Value>{shortsInfo?.shortsChallengers}</Value>
-          </DetailItem>
-        </DetailContainer>
-        <ButtonList>
-          <Button variant="secondary" onClick={goToLearnMode}>
-            연습 모드
-          </Button>
-          <Button variant="primary" onClick={goToChallengeMode}>
-            챌린지 모드
-          </Button>
-        </ButtonList>
-      </ShortsDetailContainer>
+      <ShortsContainer>
+        <VideoBox
+          src={shortsInfo?.shortsLink}
+          crossOrigin="anonymous"
+          onClick={handleVideoClick}
+        ></VideoBox>
+        {showDetails && (
+          <DetailContainer>
+            <CancelIcon>
+              <CancelPresentation onClick={handleVideoClick} />
+            </CancelIcon>
+            <Details>
+              <Detail
+                icon={<MusicNote />}
+                text={shortsInfo?.shortsTitle}
+                fontWeight="bold"
+                fontSize="22px"
+              ></Detail>
+              <Detail text={shortsInfo?.shortsDirector} fontSize="18px"></Detail>
+              <Detail
+                icon={<TimerOutlined />}
+                text={String(shortsInfo?.shortsTime) + "초"}
+              ></Detail>
+              <Detail
+                icon={<EmojiPeople />}
+                text={String(shortsInfo?.shortsChallengers) + "명의 챌린저"}
+              ></Detail>
+            </Details>
+            <ButtonContainer>
+              <RouteButton onClick={goToLearnMode}>연습모드</RouteButton>
+              <RouteButton onClick={goToChallengeMode}>챌린지모드</RouteButton>
+            </ButtonContainer>
+          </DetailContainer>
+        )}
+      </ShortsContainer>
     </Container>
   );
 };
@@ -80,55 +84,76 @@ const ShortsDetailPage = () => {
 export default ShortsDetailPage;
 
 const Container = styled.div`
-  box-sizing: border-box;
   width: 100%;
   height: 100%;
 `;
-const ShortsDetailContainer = styled.div`
+
+const ShortsContainer = styled.div`
+  position: relative;
   display: flex;
   justify-content: center;
-  flex-direction: column;
+  height: 90%;
   width: 100%;
-  height: 100%;
 `;
 
 const VideoBox = styled.video`
-  height: 70%;
-  width: 100%;
+  height: 100%;
+  cursor: pointer;
 `;
 
 const DetailContainer = styled.div`
-  height: 30%;
-  width: 100%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: rgba(255, 255, 255, 0.8);
+  z-index: 1;
+  padding: 10px;
 `;
 
-const DetailItem = styled.div`
+const CancelIcon = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: start;
+  justify-content: flex-end;
 `;
 
-const Label = styled.span`
-  font-weight: bold;
-  margin-right: 10px;
-  min-width: 100px; // 최소 너비 설정
-`;
-
-const Value = styled.span`
-  flex-grow: 1;
-  text-align: center;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-`;
-
-const ButtonList = styled.div`
+const Details = styled.div`
   display: flex;
   justify-content: center;
-  padding: 5px;
+  flex-direction: column;
+  align-items: center;
+`;
 
-  button {
-    margin: 0 10px;
-    padding: 8px 20px;
+interface DetailType {
+  text?: string;
+  fontSize?: string;
+  fontWeight?: string;
+  icon?: JSX.Element;
+}
+
+const Detail = ({ icon, text, fontSize, fontWeight }: DetailType) => {
+  return (
+    <div style={{ fontSize: fontSize, fontWeight: fontWeight }}>
+      {icon} {text}
+    </div>
+  );
+};
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const RouteButton = styled.button`
+  border: 3px solid black;
+  border-radius: 20px;
+  background-color: #f3f3f3;
+  color: black;
+  padding: 8px;
+  cursor: pointer;
+  margin: 5px; 0px;
+  font-size: 14px;
+
+  &:hover {
+    background-color: #FF7EA0;; 
   }
 `;
