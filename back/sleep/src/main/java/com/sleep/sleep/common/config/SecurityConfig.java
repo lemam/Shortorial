590864c6090e6a0b18d5eab26810d7fc2ca46d22
전성수 <ke_wer@naver.com>
2024-05-20 01:46:59 +0900

    Fix: auth 수정

--- back/sleep/src/main/java/com/sleep/sleep/common/config/SecurityConfig.java
+++ back/sleep/src/main/java/com/sleep/sleep/common/config/SecurityConfig.java
@@ -43,7 +43,13 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(auth ->
                         auth.requestMatchers("/").hasRole("USER")
-                        .anyRequest().permitAll())
+                                .requestMatchers("/api/member/reissue").permitAll()
+                                .requestMatchers("/api/member/check/").permitAll()
+                                .requestMatchers("/api/member/join").permitAll()
+                                .requestMatchers("/api/member/login").permitAll()
+                                .requestMatchers("/api/shorts/topRanking").permitAll()
+
+                        .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .csrf(AbstractHttpConfigurer::disable)
                 .headers((headerConfig)->headerConfig.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
