412bccf51a79854e1993bc2414f05f283fd0bda2
lemam <ije000131@gmail.com>
2024-11-27 15:55:55 +0900

    Feat: 영상에 마우스 오버 시 미리보기 재생되는 작업 주석 작성
    - Uncaught (in promise) NotAllowedError: play() failed because the user didn't interact with the document first. 오류 해결 과정 작성

--- front/src/components/shorts/ShortsCard.tsx
+++ front/src/components/shorts/ShortsCard.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 import { Card, CardVideo, CardTitle, CardSubTitle, CardVideoSkeleton } from "./style";
 import { Shorts } from "../../constants/types";
@@ -8,26 +8,40 @@ interface ShortsCardProps {
 }
 
 const ShortsCard = ({ shortsInfo }: ShortsCardProps) => {
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const videoRef = useRef<HTMLVideoElement>(null);
 
   const handleClick = () => {
     alert("모달이 열립니다.");
   };
 
+  const [context, setContext] = useState<AudioContext | null>(null);
+
+  // 영상에 마우스 올리면 재생되도록 하는 거 만드는 중
+  // 근데 사용자 인터렉션 없이 재생하는 거 안 됨
+  // mute하면 해결할 수 있다.
+  // 이후 mute를 store에서 통합 관리해서 다른 곳에서 음소거를 해제하면 계속 유지되도록 하자.
+  // 마우스를 떼면 0초로 돌아가는 기능도 필요
+  // 미리보기는 전체는 아니고 5초 후에 멈추는 걸로 하자.
+  const hanldeMouseEnter = () => {
+    videoRef.current?.play();
+    console.log(shortsInfo.shortsTitle);
+  };
+
   return (
-    <>
-      <Card onClick={handleClick}>
-        {isLoading && <CardVideoSkeleton />}
-        <CardVideo
-          src={shortsInfo.shortsLink}
-          crossOrigin="anonymous"
-          onLoadedData={() => setIsLoading(false)}
-          style={{ display: `${isLoading ? "none" : "inline"}` }}
-        ></CardVideo>
-        <CardTitle>{shortsInfo.shortsTitle}</CardTitle>
-        <CardSubTitle>챌린저 {shortsInfo.shortsChallengers}명</CardSubTitle>
-      </Card>
-    </>
+    <Card onClick={handleClick} onMouseEnter={hanldeMouseEnter}>
+      {isLoading && <CardVideoSkeleton />}
+      <CardVideo
+        muted
+        src={shortsInfo.shortsLink}
+        crossOrigin="anonymous"
+        onLoadedData={() => setIsLoading(false)}
+        style={{ display: `${isLoading ? "none" : "inline"}` }}
+        ref={videoRef}
+      ></CardVideo>
+      <CardTitle>{shortsInfo.shortsTitle}</CardTitle>
+      <CardSubTitle>챌린저 {shortsInfo.shortsChallengers}명</CardSubTitle>
+    </Card>
   );
 };
 
