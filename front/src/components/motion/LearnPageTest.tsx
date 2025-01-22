632b00843a3215f1429ab4ed116893f0c3a6697d
lemam <ije000131@gmail.com>
2025-01-22 15:26:40 +0900

    Test: 화면에 크기가 바뀔 때마다 영상의 크기를 재계산하도록 구현

--- front/src/components/motion/LearnPageTest.tsx
+++ front/src/components/motion/LearnPageTest.tsx
@@ -16,7 +16,7 @@ function LearnPageTest() {
   const { userPermission } = useCameraStore();
   const params = useParams();
 
-  const videoSectionRef = useRef<HTMLDivElement>(null);
+  const timestampSectionRef = useRef<HTMLDivElement>(null);
   const [videoSize, setVideoSize] = useState<Size>({ width: 0, height: 0 });
 
   // 쇼츠 영상 데이터 가져오기
@@ -27,50 +27,57 @@ function LearnPageTest() {
     }
   }, [params.shortsNo]);
 
+  // 비디오 요소 크기 계산
+  // TimestampSection 크기를 제외한 컨테이너 넓이에 적절한 비디오 너비/높이를 계산하여 videoSize에 저장한다.
+  const calcVideoSize = useCallback(() => {
+    const section = timestampSectionRef.current;
+
+    if (section && videoInfo) {
+      const totalWidth = window.innerWidth - section.offsetWidth; // 사용 가능한 VideoSection 너비
+      const width = totalWidth / 2;
+      const height = section.offsetHeight;
+      const ratioWidth = (height * 9) / 16;
+
+      // 사용 가능한 너비가 비율에 맞춰 계산한 너비보다 넓은 경우 화면에 꽉차게 출력한다.
+      if (width >= ratioWidth) setVideoSize({ width: ratioWidth, height });
+      else setVideoSize({ width, height: (width * 16) / 9 });
+    }
+  }, [videoInfo]);
+
   useEffect(() => {
     loadVideo();
   }, [loadVideo]);
 
-  // 비디오 요소 크기 계산
-  // TODO: 화면이 바뀔 때마다 다시 게산하도록 수정해야 한다.
   useEffect(() => {
-    if (videoSectionRef.current && videoInfo) {
-      const width = videoSectionRef.current.offsetWidth / 2;
-      const height = videoSectionRef.current.offsetHeight;
-      const ratioWidth = (height * 9) / 16;
+    calcVideoSize();
+    window.addEventListener("resize", calcVideoSize);
 
-      if (width >= ratioWidth) setVideoSize({ width: ratioWidth, height });
-      else setVideoSize({ width, height: (width * 16) / 9 });
-
-      // console.log("Width:", width, "Height:", height);
-    }
-  }, [videoInfo]);
+    return () => window.addEventListener("resize", calcVideoSize);
+  }, [calcVideoSize]);
 
   return (
     <Layer>
       {!userPermission && (
         <div>카메라 접근이 차단되었습니다. 상단 아이콘을 클릭하여 접근을 허용 후 새로고침해주세요.</div>
       )}
-      <Container style={{ padding: "0 16px" }}>
-        <TimestampSection style={{ flexBasis: "160px" }}>
+      <Container>
+        <TimestampSection ref={timestampSectionRef}>
           <Button>0:00</Button>
           <Button>0:10</Button>
           <Button>0:20</Button>
           <Button>0:30</Button>
         </TimestampSection>
-        <VideoSection ref={videoSectionRef} style={{ flex: "1", background: "lightblue" }}>
+        <VideoSection>
           {videoInfo && (
             <VideoContainer>
-              <VideoBox
-                style={{ position: "relative", width: `${videoSize.width}px`, height: `${videoSize.height}px` }}
-              >
-                <Video src={videoInfo?.shortsLink} crossOrigin="anonymous"></Video>
+              <VideoBox style={{ width: `${videoSize.width}px`, height: `${videoSize.height}px` }}>
+                <Video src={videoInfo.shortsLink} crossOrigin="anonymous"></Video>
               </VideoBox>
             </VideoContainer>
           )}
           {/* TODO: 비디오가 켜질 때까지 기다리기... 화면에 표시해주기 */}
           <VideoContainer>
-            <VideoBox style={{ position: "relative", width: `${videoSize.width}px`, height: `${videoSize.height}px` }}>
+            <VideoBox style={{ width: `${videoSize.width}px`, height: `${videoSize.height}px` }}>
               <MotionCameraTest />
             </VideoBox>
           </VideoContainer>
@@ -101,7 +108,9 @@ const TimestampSection = styled.section`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin-right: 48px;
+  flex-basis: 160px;
+  flex-shrink: 0;
+  padding: 0 24px;
 `;
 
 const Button = styled.button`
@@ -114,7 +123,6 @@ const Button = styled.button`
 
 const VideoSection = styled.section`
   display: flex;
-  /* width: 100%; */
   justify-content: center;
 `;
 
@@ -126,6 +134,7 @@ const VideoContainer = styled.div`
 `;
 
 const VideoBox = styled.div`
+  position: relative;
   height: 100%;
   aspect-ratio: 9/16;
 `;
